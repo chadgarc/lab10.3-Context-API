05ce0833d33fa4923f938d87910464d69946e9ee
Christian Garlen <chadgarval@outlook.com>
2026-09-13 19:19:10 -0400

    Added Todo context provider

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,12 +1,15 @@
 import { FilterProvider } from './contexts/FilterContext';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { TodoContextProvider } from './contexts/TodoContext';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider>
-      <FilterProvider>
-        {children}
-      </FilterProvider>
+      <TodoContextProvider>
+        <FilterProvider>
+          {children}
+        </FilterProvider>
+      </TodoContextProvider>
     </ThemeProvider>
   );
 }
