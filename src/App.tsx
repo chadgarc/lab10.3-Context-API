8d066a077c471126dfd9d9364da76d9cd206ffcc
Christian Garlen <chadgarval@outlook.com>
2026-09-13 17:47:34 -0400

    Implementation of providers and theme to the main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
+import { Providers } from './Providers';
+import { ThemeToggleButton } from './Components/ThemeToggleButton';
 
 function App() {
-
   return (
-    <>
-    
-    </>
-  )
+    <Providers>
+      <div className="mockup-browser border border-base-300 w-full">
+        <div className="mockup-browser-toolbar">
+          <ThemeToggleButton />
+        </div>
+      </div>
+    </Providers>
+  );
 }
 
 export default App
---
+++ src/Providers.tsx
@@ -0,0 +1,9 @@
+import { ThemeProvider } from './contexts/ThemeContext';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider>
+      {children}
+    </ThemeProvider>
+  );
+}
