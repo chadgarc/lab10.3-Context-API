fd0373064e4d2f11786a2c859c4fad5e5ad3d2e6
Christian Garlen <chadgarval@outlook.com>
2026-09-13 17:46:19 -0400

    Context and provider of theme controller

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+type ThemeContextType = {
+    theme: 'light' | 'dark';
+    toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+const prevTheme = localStorage.getItem('theme') || 'light';
+
+export function ThemeProvider ({children}: {children: React.ReactNode}){
+    const [theme, setTheme] = useState<ThemeContextType['theme']>(prevTheme as 'light' | 'dark');
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => prevTheme === 'light' ? 'dark' : 'light');
+    }
+
+    useEffect(() => {
+        localStorage.setItem('theme', theme);
+    }, [theme]);
+    
+    return(
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+            <div data-theme={theme}>
+                {children}
+            </div>
+        </ThemeContext.Provider>
+    )
+}
+
+export const useThemeContext = () => {
+    const context = useContext(ThemeContext);
+    if (!context) {
+        throw new Error('useThemeContext must be used within ThemeProvider');
+    }
+    return context;
+}
